8022fb40ba230ec5e98c4d818a9a9a7df8913322
HvarZ <hvarzahar@gmail.com>
2022-06-26 16:26:08 +0300

    delete main method

--- src/MergeTwoSortedLists.java
+++ src/MergeTwoSortedLists.java
@@ -32,10 +32,4 @@ public class MergeTwoSortedLists {
             return list2;
         }
     }
-
-    public static void main(String[] args) {
-        ListNode head1 = new ListNode(1, new ListNode(2, new ListNode(4)));
-        ListNode head2 = new ListNode(1, new ListNode(3, new ListNode(4)));
-        ListNode result = mergeTwoLists(head1, head2);
-    }
 }
