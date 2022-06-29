40d68e8f086634dfcdfdea8e28704fdf81d4cb84
HvarZ <hvarzahar@gmail.com>
2022-06-29 23:57:35 +0300

    finish SearchInsertPosition problem (100% time)

---
+++ src/SearchInsertPosition.java
@@ -0,0 +1,16 @@
+public class SearchInsertPosition {
+    public static int searchInsert(int[] nums, int target) {
+        int start = 0; int finish = nums.length - 1;
+        while (start <= finish) {
+            int averagePosition = (start + finish) / 2;
+            if (nums[averagePosition] < target) {
+                start = averagePosition + 1;
+            } else if (nums[averagePosition] > target) {
+                finish = averagePosition - 1;
+            } else {
+                return averagePosition;
+            }
+        }
+        return start;
+    }
+}
