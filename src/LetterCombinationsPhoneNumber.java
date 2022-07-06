39a1d7610c6bdeb469021ede685c35a35d56e938
HvarZ <hvarzahar@gmail.com>
2022-07-06 17:07:13 +0300

    finish LetterCombinationsPhoneNumber (back track solution)

--- src/LetterCombinationsPhoneNumber.java
+++ src/LetterCombinationsPhoneNumber.java
@@ -1,35 +1,34 @@
 import java.util.*;
 
 public class LetterCombinationsPhoneNumber {
-    private final static Map<Character, List<Character>> numbersToLetters = new HashMap<>() {{
-        put('2', Arrays.asList('a', 'b', 'c'));
-        put('3', Arrays.asList('d', 'e', 'f'));
-        put('4', Arrays.asList('g', 'h', 'i'));
-        put('5', Arrays.asList('j', 'k', 'l'));
-        put('6', Arrays.asList('m', 'n', 'o'));
-        put('7', Arrays.asList('p', 'q', 'r', 's'));
-        put('8', Arrays.asList('t', 'u', 'v'));
-        put('9', Arrays.asList('w', 'x', 'y', 'z'));
+    private final static Map<Character, char[]> numbersToLetters = new HashMap<>() {{
+        put('2', new char[]{'a', 'b', 'c'});
+        put('3', new char[]{'d', 'e', 'f'});
+        put('4', new char[]{'g', 'h', 'i'});
+        put('5', new char[]{'j', 'k', 'l'});
+        put('6', new char[]{'m', 'n', 'o'});
+        put('7', new char[]{'p', 'q', 'r', 's'});
+        put('8', new char[]{'t', 'u', 'v'});
+        put('9', new char[]{'w', 'x', 'y', 'z'});
     }};
+
+    private static void backtrack(List<String> combinations, String digits, String current) {
+        if (digits.length() == current.length()) {
+            combinations.add(current);
+            return;
+        }
+        for (char letter : numbersToLetters.get(digits.charAt(current.length()))) {
+            backtrack(combinations, digits, current + letter);
+        }
+    }
 
 
     public static List<String> letterCombinations(String digits) {
-        LinkedList<String> result = new LinkedList<>();
+        List<String> result = new ArrayList<>();
         if (digits == null || digits.length() == 0) {
             return result;
         }
-
-        StringBuilder current = new StringBuilder();
-        result.add("");
-
-        while (result.peek().length() < digits.length()) {
-            current.append(result.poll());
-
-            for (char c : numbersToLetters.get(digits.charAt(current.length()))) {
-                result.add(current.toString() + c);
-            }
-            current.setLength(0);
-        }
+        backtrack(result, digits, "");
         return result;
     }
 }
