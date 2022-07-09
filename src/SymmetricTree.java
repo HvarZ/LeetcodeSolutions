19e0f8154a8e9b63875850c4cd0a05054d2e8368
HvarZ <hvarzahar@gmail.com>
2022-07-09 21:12:26 +0300

    finish SymmetricTree problem (100%)

---
+++ src/SymmetricTree.java
@@ -0,0 +1,40 @@
+public class SymmetricTree {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean dive(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null) {
+            return false;
+        }
+        if (left.val != right.val) {
+            return false;
+        }
+        return dive(left.left, right.right) && dive(left.right, right.left);
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return dive(root.left, root.right);
+    }
+}
