521daf083dda25646fad3204d3d66667613f26cb
hvarz <hvarzahar@gmail.com>
2022-09-06 12:25:00 +0300

    solve SearchInsertPosition problem

--- src/Algorithm/First/Day1/SearchInsertPosition.java
+++ src/Algorithm/First/Day1/SearchInsertPosition.java
@@ -1,3 +1,5 @@
+package Algorithm.First.Day1;
+
 public class SearchInsertPosition {
     public static int searchInsert(int[] nums, int target) {
         int start = 0; int finish = nums.length - 1;
