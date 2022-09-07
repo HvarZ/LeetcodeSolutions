1e0d21dcb47633a879a3178551cdfe2c6dc2294f
hvarz <hvarzahar@gmail.com>
2022-09-08 00:03:43 +0300

    solve TwoSum2InputArrayIsSorted problem

--- src/Algorithm/First/Day3/TwoSum2InputArrayIsSorted.java
+++ src/Algorithm/First/Day3/TwoSum2InputArrayIsSorted.java
@@ -4,28 +4,15 @@ import java.util.Arrays;
 
 public class TwoSum2InputArrayIsSorted {
     public int[] twoSum(int[] numbers, int target) {
-        for (int i = 0; i < numbers.length - 1; ++i) {
-            int index = binarySearch(numbers, target - numbers[i], i + 1);
-            if (index != -1) {
-                return new int[]{i + 1, index + 1};
+        int start = 0, finish = numbers.length - 1;
+        while (numbers[start] + numbers[finish] != target) {
+            if (numbers[start] + numbers[finish] > target) {
+                finish--;
+            }
+            else {
+                start++;
             }
         }
-        return new int[0];
-    }
-
-    public int binarySearch(int[] numbers, int target, int start) {
-        int finish = numbers.length - 1;
-
-        while (start <= finish) {
-            int middle = (start + finish) / 2;
-            if (numbers[middle] == target) {
-                return middle;
-            } else if (numbers[middle] < target) {
-                start = middle + 1;
-            } else {
-                finish = middle - 1;
-            }
-        }
-        return -1;
+        return new int[]{start + 1, finish + 1};
     }
 }
